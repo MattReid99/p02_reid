eadd9ea1d17d814c87be58adb33dcc44781182a8
Matthew Reid <merknicks11@gmail.com>
2019-06-13 18:46:13 -0400

    ball bounces forever

--- BouncingBall/GameScene.swift
+++ BouncingBall/GameScene.swift
@@ -11,22 +11,29 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball : SKSpriteNode?
+    
     override func didMove(to view: SKView) {
         
     }
     
     override func sceneDidLoad() {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        let ball = SKSpriteNode(imageNamed: "basketball.png")
-        ball.position = CGPoint(x: 150, y: 130)
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
-        ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.angularVelocity = 25
-        ball.physicsBody?.restitution = 0.95
-        ball.physicsBody?.affectedByGravity = true
-        ball.physicsBody?.velocity = 30.5
+        ball = SKSpriteNode(imageNamed: "basketball.png")
+        ball?.position = CGPoint(x: 150, y: 130)
+        ball?.physicsBody = SKPhysicsBody(circleOfRadius: (ball?.size.width)! / 2)
+        ball?.physicsBody?.isDynamic = true
+        ball?.physicsBody?.angularVelocity = 100
+        ball?.physicsBody?.linearDamping = 0.0
+        ball?.physicsBody?.angularDamping = 0.0
+        ball?.physicsBody?.restitution = 1.0
+        ball?.physicsBody?.friction = 0.0
+        ball?.physicsBody?.affectedByGravity = true
+        //ball?.physicsBody?.velocity = CGVector.init(dx: 15.0, dy: 15.0)
         
-        addChild(ball)
+        addChild(ball!)
     }
+    
+
 
 }
